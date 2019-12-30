4e024b55800335ac9bbb932b9d95e09b7f8cbed5
Itamar Hassin <ihassin@mac.com>
2019-12-30 15:59:29 -0500

    Moved to yarn, installed enzyme

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,12 +3,6 @@ import { render } from '@testing-library/react';
 import FragileToAgile from './FragileToAgile';
 import {shallow} from 'enzyme';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<FragileToAgile />);
-  const linkElement = getByText(/Fragile to agile/i);
-  expect(linkElement).toBeInTheDocument();
-});
-
 it('renders the heading', () => {
   const wrapper = shallow(<FragileToAgile/>);
    expect(wrapper.find('h1').text()).toBe('Fragile to agile');
