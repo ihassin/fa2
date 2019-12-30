037225a862f1252cdf1574c15a9f62f21dc6959d
Itamar Hassin <ihassin@mac.com>
2019-12-30 10:15:33 -0500

    Simplified, renamed app

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import FragileToAgile from './FragileToAgile';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const { getByText } = render(<FragileToAgile />);
+  const linkElement = getByText(/Fragile to agile/i);
   expect(linkElement).toBeInTheDocument();
 });
 
---
+++ src/FragileToAgile.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FragileToAgile: React.FC = () => {
+    return (
+        <div>
+            <h1>Fragile to agile</h1>
+        </div>
+    );
+};
+
+export default FragileToAgile;
